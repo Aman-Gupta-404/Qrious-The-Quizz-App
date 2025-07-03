dc3de915d04ecfe9f44472927bb8ab91f13f71d4
Aman-Gupta-404 <ag9139563@gmail.com>
2025-07-03 15:36:40 +0530

    added media queries

---
+++ src/data/questions.js
@@ -0,0 +1,59 @@
+const quiz = [
+  {
+    question: "What is the capital city of France?",
+    options: ["Berlin", "Madrid", "Paris", "Rome"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "Who wrote the play 'Romeo and Juliet'?",
+    options: [
+      "William Wordsworth",
+      "William Shakespeare",
+      "Jane Austen",
+      "Mark Twain",
+    ],
+    correctAnswer: "William Shakespeare",
+  },
+  {
+    question: "What is the largest planet in our Solar System?",
+    options: ["Earth", "Mars", "Jupiter", "Saturn"],
+    correctAnswer: "Jupiter",
+  },
+  {
+    question: "In which year did the World War II end?",
+    options: ["1942", "1945", "1939", "1950"],
+    correctAnswer: "1945",
+  },
+  {
+    question: "Which element has the chemical symbol 'O'?",
+    options: ["Oxygen", "Gold", "Osmium", "Oxide"],
+    correctAnswer: "Oxygen",
+  },
+  {
+    question: "What is the smallest prime number?",
+    options: ["0", "1", "2", "3"],
+    correctAnswer: "2",
+  },
+  {
+    question: "Which country is known as the Land of the Rising Sun?",
+    options: ["China", "South Korea", "Thailand", "Japan"],
+    correctAnswer: "Japan",
+  },
+  {
+    question: "What is the main language spoken in Brazil?",
+    options: ["Spanish", "Portuguese", "English", "French"],
+    correctAnswer: "Portuguese",
+  },
+  {
+    question: "How many continents are there on Earth?",
+    options: ["5", "6", "7", "8"],
+    correctAnswer: "7",
+  },
+  {
+    question: "What is the process by which plants make their food?",
+    options: ["Respiration", "Photosynthesis", "Transpiration", "Germination"],
+    correctAnswer: "Photosynthesis",
+  },
+];
+
+export default quiz;
